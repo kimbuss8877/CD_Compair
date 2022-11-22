bfa19298deb3ca90078a56c98f3182ca677c7621
kimbuss8877 <98803862+kimbuss8877@users.noreply.github.com>
2022-11-23 00:31:43 +0900

    221123
    12

--- CD_Serial/CD_Serial/Form1.cs
+++ CD_Serial/CD_Serial/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,35 @@ namespace CD_Serial
         public Form1()
         {
             InitializeComponent();
+            Current_Directory = System.IO.Directory.GetCurrentDirectory();
+            Current_Directory = Current_Directory + "\\" + "Save.pbt";
         }
+        public int Count;
+        private string Current_Directory;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileExists_Process();
+            FileStream FFs = new FileStream(Current_Directory, FileMode.Open);
+            BinaryWriter Bw = new BinaryWriter(FFs);
+
+            Count = Convert.ToInt32(textBox1.Text);
+
+            Bw.Write(Count);
+
+            Bw.Close();
+            FFs.Close();
+            MessageBox.Show("발행 완료");
+        }
+
+        private void FileExists_Process()
+        {
+            DirectoryInfo di = new DirectoryInfo(Current_Directory);
+            if (di.Exists == false)
+            {
+                System.IO.FileStream FFs = new System.IO.FileStream(Current_Directory, FileMode.Create);
+                FFs.Close();
+            }
+        }
+             
     }
 }
